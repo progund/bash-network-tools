f66154dd73b4db593d7b6b82bbb8668c857ea9c3
Henrik Sandklef <hesa@sandklef.com>
2017-02-05 22:17:04 +0100

    Better err handling

--- rcheck_domain.sh
+++ rcheck_domain.sh
@@ -2,8 +2,11 @@
 
 DOMAIN=$1
 
-if [ "$DOMAIN" = "" ]
+if [ "$DOMAIN" != "" ]
 then
        dig +short -x $(dig +short $DOMAIN | grep "^[0-9]*\.") ;
        exit $?
+else
+    echo "Missing domain" 1>&2
+    exit 1
 fi
